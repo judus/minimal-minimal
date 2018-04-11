87337899eeaf197e0e1c62700a4b46192af6d87c
Julien Duseyau <julien.duseyau@gmail.com>
2018-04-11 23:02:45 +0200

    Defined Logger::lineFormat

--- src/Providers/LoggerProvider.php
+++ src/Providers/LoggerProvider.php
@@ -1,21 +1,53 @@
 <?php namespace Maduser\Minimal\Framework\Providers;
 
-use Maduser\Minimal\Log\Logger;
 use Maduser\Minimal\Framework\Facades\Config;
+use Maduser\Minimal\Framework\Facades\Request;
+use Maduser\Minimal\Log\Logger;
 
+/**
+ * Class LoggerProvider
+ *
+ * @package Maduser\Minimal\Framework\Providers
+ */
 class LoggerProvider extends AbstractProvider
 {
     /**
-     * @return mixed
+     * @return Logger
      */
     public function resolve()
     {
-        return $this->singleton('Log',
-            new Logger(
-                PATH . Config::storage('logs'),
-                Config::log('level'),
-                Config::log('benchmarks')
-            )
+        $logger = new Logger(
+            PATH . Config::storage('logs'),
+            Config::log('level'),
+            Config::log('benchmarks')
         );
+
+        $logger->setLineFormat(function($level, $msg, $data) use ($logger) {
+            return
+                strftime('%Y-%m-%d %H:%M:%S')
+                . ' | ' . $logger::LEVELS[$level]
+                . ' | ' . $this->getRemoteIp()
+                . ' | ' . $this->getRequestMethod()
+                . ' | ' . $msg
+                . PHP_EOL;
+        });
+
+        return $this->singleton('Log', $logger);
+    }
+
+    /**
+     * @return string
+     */
+    private function getRemoteIp()
+    {
+        return str_pad(Request::getIp(), 12, ' ');
+    }
+
+    /**
+     * @return string
+     */
+    private function getRequestMethod()
+    {
+        return str_pad(Request::getRequestMethod(), 6, ' ');
     }
 }
